f89a6ce937540f1fc06d27ed91bed926ffa76208
leoleil <leoleil@163.com>
2020-05-15 18:23:36 +0800

    fix<blog>:fix the bug of opening blog

--- service/sblog/sblog.go
+++ service/sblog/sblog.go
@@ -5,71 +5,76 @@ import (
 	"magic_space/module/user"
 )
 
-func UploadBlog(title, body, key string) (err error)  {
+func UploadBlog(title, body, key string) (err error) {
 	u, err := user.QueryUserByKey(key)
-	if err != nil{
+	if err != nil {
 		return err
 	}
 	entity := blog.BlogEntity{
-		Title: title,
-		Body: body,
-		Nice: 0,
-		PV: 0,
+		Title:    title,
+		Body:     body,
+		Nice:     0,
+		PV:       0,
 		UserName: u.Username,
-		UserId: u.Id,
+		UserId:   u.Id,
 	}
 	err = blog.InsertBlog(entity)
 	return err
 }
 
-func GetBlogListByPage(page, limit int64)(b  []blog.BlogEntity, pageNum, endPageNum int64, err error)  {
+func GetBlogListByPage(page, limit int64) (b []blog.BlogEntity, pageNum, endPageNum int64, err error) {
 	n, err := blog.CountBlog()
-	if err != nil{
+	if err != nil {
 		return
 	}
-	endPageNum = n / limit + 1
+	endPageNum = n/limit + 1
 	b, err = blog.SelectBlogListByPage(page, limit)
-	if err != nil{
+	if err != nil {
 		return
 	}
 	pageNum = page
 	return
 }
 
-func UpdateBlog(id int64, title, body, key string) (err error)  {
+func UpdateBlog(id int64, title, body, key string) (err error) {
 	u, err := user.QueryUserByKey(key)
-	if err != nil{
+	if err != nil {
 		return err
 	}
 	entity := blog.BlogEntity{
-		Id: id,
-		Title: title,
-		Body: body,
-		Nice: 0,
-		PV: 0,
+		Id:       id,
+		Title:    title,
+		Body:     body,
+		Nice:     0,
+		PV:       0,
 		UserName: u.Username,
-		UserId: u.Id,
+		UserId:   u.Id,
 	}
 	err = blog.UpdateBlogById(entity)
 	return err
 }
 
-func DeleteBlog(id int64, key string) (err error)  {
+func DeleteBlog(id int64, key string) (err error) {
 	u, err := user.QueryUserByKey(key)
-	if err != nil{
+	if err != nil {
 		return err
 	}
 	err = blog.DeleteBlogByIdAndUserId(id, u.Id)
 	return err
 }
 
-func OpenBlog(id int64, key string)(b blog.BlogEntity, edit bool, err error) {
-	u, err := user.QueryUserByKey(key)
-	if err != nil{
+func OpenBlog(id int64, key string) (b blog.BlogEntity, edit bool, err error) {
+	b, err = blog.SelectBlogById(id)
+	if key == "" {
+		edit = false
 		return
 	}
-	b, err = blog.SelectBlogById(id)
-	if b.UserId == u.Id{
+	u, err := user.QueryUserByKey(key)
+	if err != nil {
+		return
+	}
+
+	if b.UserId == u.Id {
 		edit = true
 	}
 	return
